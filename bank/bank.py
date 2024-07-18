5946f719fc21eeb072a3cfee7f9344a8b872f7b0
yasin <yasin.DN@yas.com>
2024-07-18 16:19:36 +0430

    porojet 1

---
+++ bank/bank.py
@@ -0,0 +1,390 @@
+import time
+print("welcoam t Car kaneh")  
+
+time.sleep(3.5) 
+class Car_kaneh():
+  #zharid parche
+  '''import m'''
+
+  def Adres(self):
+      s = str(input("do you adres skayate mishahid: "))
+      if s == "no":
+        print("bay bay")
+        
+      if s == "yes":
+        print("adres:k shreate")
+        
+      q = str(input('reseneden: '))
+      if q == "no":
+        time.sleep(3.5)
+        q = str(input('reseneden: '))
+      
+      if q == "yes":
+        time.sleep(3.5)
+        print("wecome to skayate")
+
+  def Shyaty(self):
+      print('noe parche:')
+      time.sleep(2.4)
+
+      s = str(input('noe parche; parche, katan, jin, nakee: '))
+
+      if s == 'parche':
+        time.sleep(2.4)
+        print('motevage shodam; go to room 1')
+      
+      if s == "katan":
+        time.sleep(2.4)
+        print('motevage shodam; go to room 2')
+
+      if s == 'jin':
+        time.sleep(2.4)
+        print('motevage shodam go to room 3')
+
+      if s == "nakee":
+        time.sleep(2.4)
+        print('motevage shodam go to room 4')
+    #parcheh:parche 
+  import room
+  def Tamir(self):
+      print('noe parche: parche, katan, jin, nakee')
+      time.sleep(4.6)
+
+      q = str(input('noe parche: '))
+
+      if q == 'parche':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+
+          if u == 'no':
+            str(input('az zhadamat raze bodid; yes vs no '))
+
+
+      if q == 'katan':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+             
+          if u == 'no':         
+            print('noe parche: parche, katan, jin, nakee')
+
+            q = str(input('noe parche: '))
+
+      if q == 'jin':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+             
+          if u == 'no':
+             print('noe parche: parche, katan, jin, nakee')
+
+      q = str(input('noe parche: '))
+
+      if q == 'katan':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+             
+          if u == 'no':         
+           print('noe parche: parche, katan, jin, nakee')
+
+           q = str(input('noe parche: '))
+
+      if q == 'nakee':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+             
+          if u == 'no':
+           print('noe parche: parche, katan, jin, nakee')
+
+           q = str(input('noe parche: '))
+
+      if q == 'jin':
+        print('moshkle:kutah kardan, parge, dorost kardan zip, mark zadan')
+
+        w = str(input('moshkelash che hast: '))
+
+        if w == 'kutah':
+          print('zhymat: 20sent')
+          time.sleep(2.4)
+          t = int(input('chand SM kutah konam: '))
+          e = w * 20
+          print(e)
+
+        if w == 'parge':
+          print('har 1m/s = 2 sent')
+          time.sleep(4.6)
+          r = int(input('chand m/s parge darad: '))
+          if r < 2:
+            print('0/5 m/s parche neyaz ast')
+          if r < 5:
+            print('1 m/s parche neyaz ast')
+          if r < 10:
+            print('1/5 m/s parche neyaz ast')
+          y = r * 2
+          print(y)
+
+        if w == 'zip':
+          u = str(input('do moshkel you jip pirahan ast; yes vs no: '))
+
+          if u == 'yes':
+            print('dast moz = 15')
+            time.sleep(3.5)
+            i = int(input('pirahan shoma chand zip dard: '))
+            o = i * 15
+            print(o)
+
+          if u == 'no':
+            p = str(input('do moshkel you jip sharvar ast; yes vs no: '))
+
+            if u == 'yes':
+             print('dast moz = 15')
+             time.sleep(3.5)
+             a = int(input('pirahan shoma chand zip dard: '))
+             s = i * 15
+             print(o)
+
+
+          
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+            
+          
+
+
+
+
+
+
+
+      
+    
+
+yasin = Car_kaneh()
+yasin.Adres()
+yasin.Shyaty()
+yasin.Tamir()
